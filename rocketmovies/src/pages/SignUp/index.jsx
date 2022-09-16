1f00cda4c77961650b9c8919d78d94e7afe31b50
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2022-09-16 10:25:44 -0300

    Sign Up functionality added

--- rocketmovies/src/pages/SignUp/index.jsx
+++ rocketmovies/src/pages/SignUp/index.jsx
@@ -1,12 +1,40 @@
+import { useState }  from 'react'
+import { api } from '../../services/api'
+
 import { Container, Form, BackgroudImage } from './styles'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
  import { FiMail, FiLock, FiUser } from 'react-icons/fi'
  
  export function SignUp() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const navigate = useNavigate()
+
+    function handleSigUp(){
+        if(!name || !email || !password){
+            return alert('Please fill all of the fields')
+        }
+
+        api.post('/users', { name, email, password})
+        .then(() => {
+            alert('User successfully registered')
+            navigate('/')
+        })
+        .catch(error => {
+            if(error.response){
+                alert(error.response.data.message)
+            } else {
+                alert('Not possible to register')
+            }
+        })
+    }
+
      
      return(
          <Container>
@@ -18,11 +46,11 @@ import { Link } from 'react-router-dom'
 
                 <h2>Faça seu login</h2>
 
-                <Input icon={FiUser} placeholder="text" type="Insert your name"/>
-                <Input icon={FiMail} placeholder="E-mail" type="email"/>
-                <Input icon={FiLock} placeholder="Password" type="password"/>
+                <Input icon={FiUser} placeholder="Name" type="Insert your name" onChange = {event => setName(event.target.value)}/>
+                <Input icon={FiMail} placeholder="E-mail" type="email" onChange = {event => setEmail(event.target.value)}/>
+                <Input icon={FiLock} placeholder="Password" type="password" onChange = {event => setPassword(event.target.value)}/>
 
-                <Button title='Register' className='log-button'/>
+                <Button title='Register' className='log-button' onClick={handleSigUp}/>
                 
                 <Link to='/'>Back to Login</Link>
             </Form>
